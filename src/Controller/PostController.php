9dbe47ac3d53f1513b9434203755d73156dc2445
Jorge Monteiro <dev.henrmont@gmail.com>
2022-10-26 17:14:10 -0300

    sistema de add post

---
+++ src/Controller/PostController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Post;
+use App\Service\DataFormat;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+
+#[Route('/api')]
+class PostController extends AbstractController
+{
+    #[Route('/create/post', name: 'create_post')]
+    public function createPost(ManagerRegistry $doctrine, Request $request, DataFormat $df, SerializerInterface $serializer): Response
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+        
+        $con = $doctrine->getConnection();
+        $request = $df->transformJsonBody($request);
+
+        try {
+            $con->beginTransaction();
+
+            $post = new Post();
+            $post->setTitle($request->get('title'));
+            $post->setSubtitle($request->get('subtitle'));
+            $post->setContent($request->get('content'));
+            $post->setActive(false);
+            $post->setCreatedAt(new \DateTimeImmutable());
+            $post->setUpdatedAt(new \DateTimeImmutable());
+            $doctrine->getManager()->persist($post);
+            $doctrine->getManager()->flush();
+
+            $con->commit();
+
+            $serialized = $serializer->serialize([
+                'message'   => 'Post criado com sucesso.',
+                'status'    => true
+            ],'json');
+            return JsonResponse::fromJsonString($serialized);
+        } catch (\Exception $e) {
+            $con->rollback();
+            $serialized = $serializer->serialize([
+                'message'   => 'Erro no sistema.',
+                'status'    => false
+            ],'json');
+            return JsonResponse::fromJsonString($serialized);
+        }
+    }
+}
---
+++ src/Entity/Post.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PostRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PostRepository::class)]
+class Post
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $title = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $subtitle = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $content = null;
+
+    #[ORM\Column]
+    private ?bool $active = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $created_at = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $updated_at = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getSubtitle(): ?string
+    {
+        return $this->subtitle;
+    }
+
+    public function setSubtitle(string $subtitle): self
+    {
+        $this->subtitle = $subtitle;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function isActive(): ?bool
+    {
+        return $this->active;
+    }
+
+    public function setActive(bool $active): self
+    {
+        $this->active = $active;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $created_at): self
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeImmutable
+    {
+        return $this->updated_at;
+    }
+
+    public function setUpdatedAt(\DateTimeImmutable $updated_at): self
+    {
+        $this->updated_at = $updated_at;
+
+        return $this;
+    }
+}
